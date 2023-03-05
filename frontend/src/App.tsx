3d85034f3ee6b0339cbaf626876fcd6011f6cbd4
ottonsam <ottonsam@gmail.com>
2023-03-05 02:32:47 -0300

    import form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,9 @@
-import React from "react";
-import axios from "axios";
+import Form from "./components/UserGenerateForm";
 
 function App() {
-  const handleSubmit = async () => {
-    console.log("submit");
-    const data = await axios({
-      method: "post",
-      url: "http://localhost:3333/generate",
-      data: {
-        numero_usuarios: 2,
-        campos: ["nome", "sexo", "pai", "mãe"],
-      },
-    });
-
-    console.log(data);
-  };
   return (
     <div className="App">
-      <h1>hot reload</h1>
-      <button type="button" onClick={() => handleSubmit()}>
-        click
-      </button>
+      <Form />
     </div>
   );
 }
