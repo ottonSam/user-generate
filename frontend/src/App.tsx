a6467c9269add82657ac14b7e9415818ef78d1fc
ottonsam <ottonsam@gmail.com>
2023-03-04 22:49:47 -0300

    axios test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import axios from "axios";
 
 function App() {
+  const handleSubmit = async () => {
+    console.log("submit");
+    const data = await axios({
+      method: "post",
+      url: "http://localhost:3333/generate",
+      data: {
+        numero_usuarios: 2,
+        campos: ["nome", "sexo", "pai", "mãe"],
+      },
+    });
+
+    console.log(data);
+  };
   return (
     <div className="App">
       <h1>hot reload</h1>
+      <button type="button" onClick={() => handleSubmit()}>
+        click
+      </button>
     </div>
   );
 }
