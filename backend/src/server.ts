57a48d42496ac58fee156713234a1c374148cfbb
ottonsam <ottonsam@gmail.com>
2023-03-04 21:15:10 -0300

    change port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,4 +8,4 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen("3000", () => console.log("O servidor esta rodando na porta 3000"));
+app.listen("3333", () => console.log("O servidor esta rodando na porta 3333"));
