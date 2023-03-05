9f89b201a2d94fe7176011e5c55437dcb702ee4f
ottonsam <ottonsam@gmail.com>
2023-03-04 21:07:28 -0300

    up server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import _4devs from "@killovsky/4devs";
+
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  _4devs.gerar("1", false, "pessoa").then((data) => res.json(data.dados));
-});
+app.use(express.json());
+
+app.use(router);
 
 app.listen("3000", () => console.log("O servidor esta rodando na porta 3000"));
